c821bd977e06b1f5cdbf91b0699f49639d2576c6
ch9729 <karlll@naver.com>
2024-06-09 17:26:52 +0900

    2일차 수업

---
+++ src/variable/Var1.java
@@ -0,0 +1,10 @@
+package variable;       //자바 파일을 구분하기 위한 폴더
+
+public class Var1 {
+
+    public static void main(String[] args) {
+        System.out.println(20);     //변경 10 -> 20
+        System.out.println(20);     //변경 10 -> 20
+        System.out.println(20);     //변경 10 -> 20
+    }
+}
