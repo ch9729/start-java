2c7a35e7613990dcd78526569625bdf064fe5ccb
ch9729 <karlll@naver.com>
2024-06-14 00:12:06 +0900

    입력

---
+++ src/loop/ex/NestedEx3.java
@@ -0,0 +1,15 @@
+package loop.ex;
+
+public class NestedEx3 {
+
+    public static void main(String[] args) {
+        int rows = 1;
+
+        for(int i = 5; i >= rows; i--){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
